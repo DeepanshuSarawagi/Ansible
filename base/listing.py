b63017f29167e82fb250d5d9dca9080d24b21fde
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-11 21:19:38 +0530

    Python script for dynamic inventory

---
+++ base/listing.py
@@ -0,0 +1,25 @@
+from subprocess import Popen, PIPE
+import sys
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+RESULT = {}
+RESULT['all'] = {}
+PIPE = Popen(['getent', 'inventory'], stdout=PIPE, universal_newlines=True)
+RESULT['all']['hosts'] = []
+
+for line in PIPE.stdout.readlines():
+    s = line.splitlines()
+    RESULT['all']['hosts'] += s
+
+RESULT['all']['vars'] = {}
+
+if len(sys.argv) == 2 and sys.argv[1] == '--list':
+    print(json.dumps(RESULT))
+elif len(sys.argv) == 3 and sys.argv[2] == '--hosts':
+    print(json.dumps({}))
+else:
+    print("Requires an argument. Please use --list or --hosts")
